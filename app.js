4eb8804a03d7d895b1cf4207fb4626709cb5b83a
angad <angadsharma1016@gmail.com>
2018-10-06 23:19:07 +0530

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const hbs = require("express-handlebars");
+require("morgan")("dev");
+const bp = require("body-parser");
+const mongoose = require("mongoose");
+
+app.engine('hbs',hbs({defaultLayout:'main'}));
+app.set("view engine",'hbs');
+app.use(bp.json());
+app.use(bp.urlencoded({extended:false}));
+
+
+mongoose.connect(process.env.DBURL,{useNewUrlParser:true});
+mongoose.connection.once("open",()=>console.log("Connected to database"))
+.on("error",()=>console.log("Error connecting to DB"));
+
+
+app.listen(process.env.PORT || 3000,()=>console.log("Listening..."));
